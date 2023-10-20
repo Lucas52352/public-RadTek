2a8228ab75d5f40017c2c17d6bdfbcb49d8693c4
Lucas52352 <lucasrius86@gmail.com>
2023-10-20 12:12:34 -0300

    prev config

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,7 +5,7 @@ const userSchema= new mongoose.Schema({
     username: {type: String, required: true},
     email:{type: String, required: true, unique: true},
     password: {type: String, required: true, select: false},
-    cart: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
+    cart: {type: mongoose.Schema.Types.ObjectId, ref: 'products'},
     admin: {type: Boolean, default: false}
 })
 
